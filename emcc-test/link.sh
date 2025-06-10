218aa43eb0397f92054c84661bc82174b0375d77
Leo Chan <leo.chan@tokuindustry.com>
2025-06-09 19:18:45 -0600

    load resources

--- emcc-test/link.sh
+++ emcc-test/link.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
+set -e
 
 script_dir=$(dirname "$BASH_SOURCE")
 cc="${EMSDK}/upstream/emscripten/emcc"
 
 pushd "${script_dir}/.." || exit 1
-mkdir -p emcc-test/htmlout
 
-"${cc}" examples/core/core_input_gamepad.c zig-out/lib/libraylib.a \
--Izig-out/include \
--o emcc-test/htmlout/index.html \
+zig build -Dtarget=wasm32-emscripten
+
+mkdir -p emcc-test/htmlout
+cp -R examples/core/resources emcc-test/resources
+
+popd || exit 1
+
+"${cc}" ../examples/core/core_input_gamepad.c ../zig-out/lib/libraylib.a \
+-I../zig-out/include \
+-o htmlout/index.html \
 -sUSE_GLFW=3 \
 -sEXPORTED_RUNTIME_METHODS=ccall \
 -sINITIAL_MEMORY=67108864 \
+-sUSE_PTHREADS=1 \
 -sASSERTIONS=1 \
 -sASYNCIFY \
---shell-file src/shell.html
-
-popd || exit 1
+--preload-file resources \
+--shell-file ../src/shell.html
